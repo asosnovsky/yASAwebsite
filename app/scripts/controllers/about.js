d7207e1260b399fd514eaeda2f56e289f02ec5e1
ari <arielsosnovsky@gmail.com>
2015-01-16 01:45:01 -0500

    first recunstruction

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yuAsaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yuAsaApp
+ */
+angular.module('yuAsaApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yuAsaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yuAsaApp
+ */
+angular.module('yuAsaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
