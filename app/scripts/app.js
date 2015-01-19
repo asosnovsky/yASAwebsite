b9c9d44c9eea045a5c20fac689e911cb33164887
ari <arielsosnovsky@gmail.com>
2015-01-18 22:32:43 -0500

    fixed some styling issues, added animations :D

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,20 +9,26 @@
  * Main module of the application.
  */
 angular
-  .module('yuAsaApp', [
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+.module('yuAsaApp', ['ngRoute'])
+	.config(function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/main.html',
+				controller: 'MainCtrl'
+			})
+			.when('/about', {
+				templateUrl: 'views/about.html',
+				controller: 'AboutCtrl'
+			})
+			.when('/qualification', {
+				templateUrl: 'views/qualification.html',
+				controller: 'MainCtrl'
+			})
+			.when('/employment', {
+				templateUrl: 'views/employment.html',
+				controller: 'MainCtrl'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,20 @@
  * Controller of the yuAsaApp
  */
 angular.module('yuAsaApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('MainCtrl', function ($scope,$http,$sce) {
+	$scope.tumblr = {posts:[]};
+	$http.jsonp('http://api.tumblr.com/v2/blog/asayorku.tumblr.com/posts?api_key=9JEt1AQMzS7zhaLwn9I9kRlyq0MHzW7SkGxyjg3PIGmGKbC1Ek&callback=JSON_CALLBACK')
+		.success(function (data) {
+			data.response.posts.forEach(function (value) {
+				var img = value.body.substring((value.body.indexOf('<img')),(value.body.indexOf('"/>')+3));
+				console.log(img);
+				$scope.tumblr.posts.push({
+					title:value.title,
+					body:value.body.replace(/<[^>]*>/g, '').substr(0,200)+'...',
+					/*jshint camelcase: false */link:value.post_url,/*jshint camelcase: true */
+					time:value.date,
+					img: $sce.trustAsHtml(img)
+				});
+			});
+		});
+});
